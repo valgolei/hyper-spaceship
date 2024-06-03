1492cd9af7d2df78070b752e740179467f803251
valgolei <84638017+valgolei@users.noreply.github.com>
2024-06-03 20:10:37 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+let Tir_vaisseau: Sprite = null
+let Vaisseau = sprites.create(img`
+    . . . . . f . . . . . 
+    . . . f . f . f . . . 
+    . . . f 8 f 8 f . . . 
+    . f . f 8 6 8 f . f . 
+    . f . 8 6 6 6 8 . f . 
+    . f 8 8 6 6 6 8 8 f . 
+    . 8 8 6 6 6 6 6 8 8 . 
+    . 8 8 6 6 6 6 6 8 8 . 
+    8 8 8 8 8 f 8 8 8 8 8 
+    8 8 8 8 f f f 8 8 8 8 
+    8 . 8 8 8 f 8 8 8 . 8 
+    . . 8 . 8 8 8 . 8 . . 
+    . 8 2 8 . . . 8 2 8 . 
+    . 4 4 4 . . . 4 4 4 . 
+    5 5 5 5 5 . 5 5 5 5 5 
+    `, SpriteKind.Player)
+controller.moveSprite(Vaisseau, 100, 100)
+Vaisseau.setStayInScreen(true)
+scene.setBackgroundColor(12)
+Vaisseau.setPosition(80, 85)
+game.onUpdateInterval(70, function () {
+    Tir_vaisseau = sprites.createProjectileFromSprite(img`
+        2 
+        2 
+        2 
+        2 
+        2 
+        `, Vaisseau, 0, -100)
+    Tir_vaisseau.setPosition(Vaisseau.x, Vaisseau.y - 11)
+})
