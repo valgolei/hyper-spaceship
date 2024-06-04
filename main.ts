a5a1367459bab834371fad73f4f3b4522fbf5fd2
valgolei <84638017+valgolei@users.noreply.github.com>
2024-06-04 17:23:49 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,30 @@
 namespace SpriteKind {
     export const tir_vaisseau = SpriteKind.create()
+    export const tir_énemi = SpriteKind.create()
 }
 statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     sprites.destroy(status.spriteAttachedTo())
+})
+sprites.onOverlap(SpriteKind.tir_énemi, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    Vie.value += -10
 })
 sprites.onOverlap(SpriteKind.tir_vaisseau, SpriteKind.Enemy, function (sprite, otherSprite) {
     statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -1
     sprites.destroy(sprite)
 })
 let Tir_vaisseau: Sprite = null
+let tir_énemi_basique: Sprite = null
 let vie_énemi_basique: StatusBarSprite = null
 let énemi_basique: Sprite = null
+let Vie: StatusBarSprite = null
 let Vaisseau = sprites.create(img`
     . . . . . f . . . . . 
     . . . f . f . f . . . 
     . . . f 8 f 8 f . . . 
-    . f . f 8 6 8 f . f . 
-    . f . 8 6 6 6 8 . f . 
-    . f 8 8 6 6 6 8 8 f . 
+    . . . f 8 6 8 f . . . 
+    . . . 8 6 6 6 8 . . . 
+    . . 8 8 6 6 6 8 8 . . 
     . 8 8 6 6 6 6 6 8 8 . 
     . 8 8 6 6 6 6 6 8 8 . 
     8 8 8 8 8 f 8 8 8 8 8 
@@ -32,14 +39,16 @@ controller.moveSprite(Vaisseau, 90, 90)
 Vaisseau.setStayInScreen(true)
 scene.setBackgroundColor(12)
 Vaisseau.setPosition(80, 85)
-let Vie = statusbars.create(50, 1, StatusBarKind.Health)
+Vie = statusbars.create(50, 1, StatusBarKind.Health)
 Vie.setPosition(27, 118)
 let spaw_des_énemis_basiques = 0
-forever(function () {
-	
+game.onUpdateInterval(1000, function () {
+    for (let valeur of sprites.allOfKind(SpriteKind.Enemy)) {
+        valeur.setVelocity(randint(-10, 10), randint(0, 3))
+    }
 })
 forever(function () {
-    if (spaw_des_énemis_basiques > 6000) {
+    if (spaw_des_énemis_basiques > 2000) {
         spaw_des_énemis_basiques = 0
         énemi_basique = sprites.create(img`
             . d d d d d . 
@@ -53,7 +62,29 @@ forever(function () {
         énemi_basique.setPosition(randint(5, 155), 10)
         vie_énemi_basique = statusbars.create(9, 1, StatusBarKind.EnemyHealth)
         vie_énemi_basique.attachToSprite(énemi_basique, 2, 0)
-        vie_énemi_basique.max = 30
+        vie_énemi_basique.max = 20
+        énemi_basique.setStayInScreen(true)
+    }
+})
+forever(function () {
+    pause(2000)
+    for (let index = 0; index < 3; index++) {
+        for (let valeur of sprites.allOfKind(SpriteKind.Enemy)) {
+            if (Math.percentChance(50)) {
+                tir_énemi_basique = sprites.createProjectileFromSprite(img`
+                    . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . 
+                    . . . . . . . 9 . . . . . . . 
+                    . . . . . . . 9 . . . . . . . 
+                    . . . . . . . 9 . . . . . . . 
+                    . . . . . . 9 9 9 . . . . . . 
+                    . . . . . . . 9 . . . . . . . 
+                    . . . . . . . . . . . . . . . 
+                    `, valeur, randint(-30, 30), 50)
+                tir_énemi_basique.setKind(SpriteKind.tir_énemi)
+            }
+        }
+        pause(100)
     }
 })
 game.onUpdateInterval(100, function () {
@@ -65,6 +96,24 @@ game.onUpdateInterval(100, function () {
         2 
         `, Vaisseau, 0, -120)
     Tir_vaisseau.setPosition(Vaisseau.x, Vaisseau.y - 11)
+    Tir_vaisseau.setKind(SpriteKind.tir_vaisseau)
+    Tir_vaisseau = sprites.createProjectileFromSprite(img`
+        2 
+        2 
+        2 
+        2 
+        2 
+        `, Vaisseau, 0, -120)
+    Tir_vaisseau.setPosition(Vaisseau.x - 2, Vaisseau.y - 10)
+    Tir_vaisseau.setKind(SpriteKind.tir_vaisseau)
+    Tir_vaisseau = sprites.createProjectileFromSprite(img`
+        2 
+        2 
+        2 
+        2 
+        2 
+        `, Vaisseau, 0, -120)
+    Tir_vaisseau.setPosition(Vaisseau.x + 2, Vaisseau.y - 10)
     Tir_vaisseau.setKind(SpriteKind.tir_vaisseau)
 })
 game.onUpdateInterval(100, function () {
